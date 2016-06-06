f19279b0764c44d754e2f564802123ff9e9ece6e
Julian Feliciano <julsfelic@gmail.com>
2016-06-06 11:15:28 -0600

    Give registered users the ability to sign in
    * Users can sign in when given the proper email / password combo
    * They are redirected to the links index page

---
+++ spec/features/user_can_sign_in_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.feature 'User can sign in' do
+  context 'with valid info' do
+    scenario 'they are sent to the links page' do
+      user = create(:user)
+
+      visit root_path
+      click_on 'Login'
+      fill_in 'Email', with: user.email
+      fill_in 'Password', with: user.password
+      click_on 'Login'
+
+      expect(current_path).to eq '/links'
+      expect(page).to have_content 'All of the links!'
+      expect(page).to have_content "Welcome, #{user.email}!"
+      expect(page).to have_link 'Log Out', href: '/logout'
+    end
+  end
+end
